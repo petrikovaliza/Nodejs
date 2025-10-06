87d54cf743fd64df49f434d7b485facb392ccb8d
petrikovaliza <petrikovaelizaveta3@gmail.com>
2025-10-06 23:11:21 +0500

    1

---
+++ react_project/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    const divStyle = {
+      backgroundColor: '#8cd8d2ff',
+      height: '100vh'
+    };
+    return (
+      <div style={divStyle} className="App">
+        <h1 classname="text-blue">Hello React!</h1>
+      </div>
+    );
+}
+
+export default App;
